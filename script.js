9c9c97a59e312645f03aa05b5e77f848aa00a4e2
CoderwithASpecialemployer <richysrandom@gmail.com>
2025-04-20 16:19:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
     const target = document.querySelector(link.getAttribute('href'));
-    if (target) target.scrollIntoView({ behavior: 'smooth' });
-    siteNav.classList.remove('open');
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+      siteNav.classList.remove('open');
+    }
   });
 });
